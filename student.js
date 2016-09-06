5721c0e18cd8f59163a95cb6b818b5f84b1db7c6
Xtal Osc <xtalosc@gmx.com>
2016-09-06 08:34:36 -0500

    fix next button

--- student.js
+++ student.js
@@ -1,13 +1,12 @@
-console.log('students sourced');
+console.log('student.js sourced');
 
-//
-var students = [];//global array
-var index=0;
-var timer=0;
+//global variables
+var students = [];//store student objects
+var index = 0;//store current student index number
+var timer = 0;//store timer count
 
 $(document).ready(function(){
   console.log('document ready');
-
 
   //prev button onClick
   $('#prev').click( function(){
@@ -17,65 +16,77 @@ $(document).ready(function(){
       index = students.length - 1;
     }//end if
     studentData();
-  }); // end prevButton click
-  //next button onClick
-  $('#next').on('click',next);
-  var next=function(){
+  }); // end prev button
+
+
+  //next function needed for timer
+  var next = function(){
     index++;
     // wrap around to first student
     if(index >= students.length){
       index = 0;
     }//end if
     studentData();
-  }; // end prevButton click
+  }; // end next button
+  //next button onClick
+  $('#next').on('click',next);
 
 
   var buttonData = function(){
     // console.log("in buttonData");
-    for (var i = 0; i < students.length; i++) {
-
-      var btn=$("<button/>");//create button for each student
-      btn.data('buttonIndex',i);//key:buttonIndex value:i
-      btn.html(students[i].first_name);//add name to button
-      btn.on('click',addButtonId);//onClick function
-      $('#button').append(btn);//append button to DOM
+    for (var i = 0; i < students.length; i++){
+      var btn = $('<button/>');
+      //create button for each student
+      btn.data('buttonIndex', i);//key:buttonIndex value:i
+      //add first name to button
+      btn.html(students[i].first_name);
+      //button onClick - call addButtonId function
+      btn.on('click', addButtonId);
+      //append button to DOM
+      $('#button').append(btn);
       // console.log("button data",btn.data('buttonIndex',i));
-    }
+    }// end for
   }//end ButtonData
 
   var addButtonId = function(){
     // console.log("in addButtonId");
     var toGlobal = $(this).data('buttonIndex')//data that's associated with this specific button
-    index=toGlobal;
+    index = toGlobal;
     studentData();
-  }
+  }//end addButtonId
 
   //Timer automatically goes to next student after 10 seconds
   var setTimer = setInterval(function(){
+    // console.log("in setTimer");
     //after 10 sec reset timer to 0
     if (timer === 10){
       timer = 0;
-      //display next student
+      // display next student
       next();
-    }
-    else{
+    }//end if
+    else {
       timer++;
-    }
+    }//end else
   }, 1000);//1 sec
 
   var studentData = function(){
-    //loop through students
-    // console.log("in studentData");
-    var num = index+1;//current student #
-    timer=0;//moved to next student. Reset timer
+    //console.log("in studentData");
+    //moved to next student. Reset timer
+    timer = 0;
+    //number of the current student (out of a total of student.length)
+    var num = index + 1;
+    //fadeOut previous student info
     $('#output').fadeOut(function(){
-      $("#output").empty();
-      $("#output").append("Student "+num+" of "+students.length);
-      $("#output").append("<h2>"+students[index].first_name+" "+students[index].last_name+"</h2>");
-      $("#output").append("<h4>"+students[index].info+"</h4>");
-      $("#output").hide().fadeIn();
-    });//end fadeOut
-  }//end student data
+      //empty output div
+      $('#output').empty();
+      //append current student info
+      $('#output').append("Student " + num + " of " + students.length);
+      $('#output').append("<h2>" + students[index].first_name + " " + students[index].last_name + "</h2>");
+      $('#output').append("<h4>" + students[index].info + "</h4>");
+      //fadeIn the current student info
+      $('#output').hide().fadeIn();
+    });//end fadeOut function
+  }//end studentData
 
 
   $.ajax({
@@ -83,8 +94,9 @@ $(document).ready(function(){
     dataType: 'json',
     success: function(data){
       for (var i = 0; i < data.students.length; i++) {
+        //push data.students to array
         students.push(data.students[i]);
-      };
+      };//end for
       buttonData();
     }//end Ajax success function
   });//end Ajax
